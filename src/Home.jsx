83395d27a6dacd5db526ef4e5a064c802a37d184
Pawan-gautam011 <pawangautam11op@gmail.com>
2024-08-14 21:17:54 +0545

    Trying to make it more responsible

--- src/Home.jsx
+++ src/Home.jsx
@@ -36,7 +36,7 @@ const Home = () => {
 
   return (
     <>
-      <section className="max-w-2xl m-auto p-4">
+      <section className="max-w-2xl mx-auto p-4">
         <h2 className="text-2xl sm:text-4xl font-semibold mb-4 text-center text-[#F7F7F8]">Posts</h2>
         {orderedPosts.map((post) => (
           <article key={post.id} className="border border-gray-300 rounded-lg p-4 mb-10 mt-10 backdrop-blur-md bg-[#4E31AA] bg-white/10 border-white/20 shadow-[0_40px_30px_rgba(0,0,0,0.1)]">
@@ -45,19 +45,19 @@ const Home = () => {
             <PostAuthor userId={post.user} />
             <TimeAgo timestamp={post.date} />
             <Reaction post={post} />
-            <div className='flex flex-wrap sm:flex-row mt-6 items-center  '>
-              <div className='mx-auto sm:m-4'>
-                <Link to={`/posts/${post.id}`} className='inline-block p-3 text-white rounded-md bg-blue-800 hover:bg-blue-600 text-center'>
+            <div className='flex flex-col sm:flex-row mt-6 items-center'>
+              <div className='mx-auto sm:mx-4 mb-4 sm:mb-0'>
+                <Link to={`/posts/${post.id}`} className='inline-block p-3 text-white rounded-md bg-blue-800 hover:bg-blue-600 text-center w-full'>
                   View Post
                 </Link>
               </div>
-              <div className='m-2 sm:m-4'>
-                <Link to={`/editpost/${post.id}`} className='inline-block p-3 text-white rounded-md bg-blue-800 hover:bg-blue-600 text-center'>
+              <div className='mx-auto sm:mx-4 mb-4 sm:mb-0'>
+                <Link to={`/editpost/${post.id}`} className='inline-block p-3 text-white rounded-md bg-blue-800 hover:bg-blue-600 text-center w-full'>
                   Edit Post
                 </Link>
               </div>
-              <div className='m-2 sm:m-4'>
-                <MdDelete className='h-10 w-8 sm:h-10 sm:w-10 cursor-pointer text-red hover:text-red-600 mx-[117px] sm:mx-0' onClick={() => handleDelete(post.id)} />
+              <div className='mx-auto sm:mx-4'>
+                <MdDelete className='h-8 w-8 sm:h-10 sm:w-10 cursor-pointer text-red hover:text-red-600 mx-auto sm:mx-0' onClick={() => handleDelete(post.id)} />
               </div>
             </div>
           </article>
